a0dac00d07219c6ad5a6b40e511ab4b0f1e5dccf
kamojiro <tamagoma002@yahoo.co.jp>
2019-04-07 00:18:26 +0900

    文章をちゃんと読んでなかったり、プログラムを理解できてなかったりが原因だった。

--- wiki.go
+++ wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Page struct {
@@ -14,4 +16,31 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600) //filenameというp.Bodyという内容のfileを作る。最後のやつがわかんない。
+}
+
+// loadPage loads pages
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+// viewHandler HandleFunc needs this Handler
+func viewHandler(w http.ResponseWriter, r *http.Request)  {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	// http://localhost:8080/view/test
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
 }
